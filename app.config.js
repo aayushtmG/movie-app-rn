7caa06d2b63758fef3a1b55c50884777d4650452
aayushtmG <aiyoustmg@gmail.com>
2025-04-16 17:14:35 +0545

    Updated

---
+++ app.config.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "Movie Ghar",
+    slug: "mobile_movie_app",
+    android: {
+      package: "com.aayush.movieghar"
+    },
+    extra: {
+      eas: {
+        projectId: "432fb16d-adff-4217-927c-f70aeab049f0"
+      },
+      EXPO_PUBLIC_API_KEY: process.env.EXPO_PUBLIC_API_KEY,
+      EXPO_PUBLIC_APPWRITE_PROJECT_ID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+      EXPO_PUBLIC_APPWRITE_DATABASE_ID: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+      EXPO_PUBLIC_APPWRITE_COLLECTION_ID: process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID
+    },
+  },
+};
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,11 +1,11 @@
 import {Client, Databases, ID, Query } from 'react-native-appwrite'
+import  Constants from 'expo-constants';
 
 
-const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!
-const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!
+const DATABASE_ID = Constants.expoConfig?.extra?.EXPO_PUBLIC_APPWRITE_DATABASE_ID!
+const COLLECTION_ID = Constants.expoConfig?.extra?.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!
 
-
-const client = new Client().setEndpoint('https://cloud.appwrite.io/v1').setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+const client = new Client().setEndpoint('https://cloud.appwrite.io/v1').setProject(Constants.expoConfig?.extra?.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
 const database = new Databases(client)
 
 
